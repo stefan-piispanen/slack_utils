34bede3bfacf0b720c288755563112978e254a98
Stefan <stefan@ponnykatt.se>
2026-05-02 01:47:03 +0200

    feat: add the Body object at the module level, add a faulty Body object at the module level

--- tests/test_body.py
+++ tests/test_body.py
@@ -1,26 +1,45 @@
 import pytest
 from slack_utils import Body
 
+body = Body({
+        "user": {"id": "U12345"},
+        "view": {
+            "view": {},
+            "id": "1234",
+            "blocks": []
+            },
+        "trigger_id": "1234",
+        })
+
+faulty_body = Body({
+        })
+
 def test_user_id():
-    body = Body({"user": {"id": "U12345"}})
     assert body.user_id == "U12345"
 
 def test_user_id_missing():
-    body = Body({})
-    assert body.user_id is None
+    assert faulty_body.user_id is None
 
 def test_view():
-    body = Body({"view": {"view": {}}})
     assert isinstance(body.view, dict)
 
 def test_no_view():
-    body = Body({})
-    assert body.view is None
+    assert faulty_body.view is None
 
 def test_view_id():
-    body = Body({"view": {"id": "1234"}})
     assert body.view_id == "1234"
 
 def test_no_view_id():
-    body = Body({})
-    assert body.view_id is None
+    assert faulty_body.view_id is None
+
+def test_trigger_id():
+    assert body.trigger_id == "1234"
+
+def test_no_trigger_id():
+    assert faulty_body.trigger_id is None
+
+def test_blocks():
+    assert isinstance(body.blocks, list)
+
+def test_no_blocks():
+    assert faulty_body.blocks is None
