7480f19f52f35d0df68ccbf1658ca50f519adcbb
Stefan <stefan@ponnykatt.se>
2026-05-02 01:38:06 +0200

    feat: add tests for view and view_id

--- tests/test_body.py
+++ tests/test_body.py
@@ -8,3 +8,19 @@ def test_user_id():
 def test_user_id_missing():
     body = Body({})
     assert body.user_id is None
+
+def test_view():
+    body = Body({"view": {"view": {}}})
+    assert isinstance(body.view, dict)
+
+def test_no_view():
+    body = Body({})
+    assert body.view is None
+
+def test_view_id():
+    body = Body({"view": {"id": "1234"}})
+    assert body.view_id == "1234"
+
+def test_no_view_id():
+    body = Body({})
+    assert body.view_id is None
