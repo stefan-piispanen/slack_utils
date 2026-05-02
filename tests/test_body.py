d9f85923b1764a4dc2b988083c24277deb147f76
Stefan <stefan@ponnykatt.se>
2026-05-02 03:33:47 +0200

    feat: add more tests

--- tests/test_body.py
+++ tests/test_body.py
@@ -16,6 +16,26 @@ body = Body({
             "private_metadata": {},
             "state": {
                 "values": {
+                    "datepicker_id": {
+                        "datepicker_id": {
+                            "type": "datepicker",
+                            "selected_date": "2026-01-01"
+                            }
+                        },
+                    "plain_text_id": {
+                        "plain_text_id": {
+                            "type": "plain_text_input",
+                            "value": "test_text"
+                            }
+                        },
+                    "static_id": {
+                        "static_id": {
+                            "type": "static_select",
+                            "selected_option": {
+                                "value": "test_value"
+                                }
+                            }
+                        }
                     }
                 }
             },
@@ -28,7 +48,7 @@ body = Body({
             "id": "C1234"
             },
         "container": {},
-        "actions": [{"value": {}}],
+        "actions": [{"value": "{}"}],
         "message": {}
         })
 
@@ -96,8 +116,58 @@ def test_no_user_team_id():
     assert faulty_body.user_team_id is None
 
 def test_name():
-    assert body.user_name == "Test"
+    assert body.name == "Test"
 
 def test_no_name():
     assert faulty_body.name is None
 
+def test_user_name():
+    assert body.user_name == "Test"
+
+def test_no_user_name():
+    assert faulty_body.user_name is None
+
+def test_channel_id():
+    assert body.channel_id == "C1234"
+
+def test_no_channel_id():
+    assert faulty_body.channel_id is None
+
+def test_values():
+    assert isinstance(body.values, dict)
+
+def test_no_values():
+    assert faulty_body.values is None
+
+def test_container():
+    assert isinstance(body.container, dict)
+
+def test_no_container():
+    assert faulty_body.container is None
+
+def test_actions():
+    assert isinstance(body.actions, dict)
+
+def test_no_actions():
+    assert faulty_body.actions is None
+
+def test_value():
+    assert isinstance(body.value, dict)
+
+def test_no_value():
+    assert faulty_body.value is None
+
+def test_message():
+    assert isinstance(body.message, dict)
+
+def test_no_message():
+    assert faulty_body.message is None
+
+def test_get_value_datepicker():
+    assert body.get_value("datepicker_id") == "2026-01-01"
+
+def test_get_value_plain_text():
+    assert body.get_value("plain_text_id") == "test_text"
+
+def test_get_value_static_select():
+    assert body.get_value("static_id") == "test_value"
